faaa469a540f528142f54db977db3d77288caa0d
mfdsix <mfdsix.1nd0@gmail.com>
2022-08-04 08:08:46 +0700

    fix xls to xlsx

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PIL import Image
 
 st.set_page_config(layout='centered')
-df = pd.read_excel("./all.xls", engine='openpyxl')
+df = pd.read_excel("./all.xlsx")
 
 st.title("INSIGHTS MENGENAI MEREK DAGANG PDKI")
 
